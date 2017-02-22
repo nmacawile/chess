5359689597ff1a3cc8a30702b42ff47589958543
nmacawile <nmacawile@gmail.com>
2017-02-22 17:55:00 +0800

    finish Knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -3,6 +3,23 @@ require "pieces/piece"
 class Knight < Piece
 	def find_legal_moves
 		self.legal_moves = []
-		
+		knight_moves
+	end
+
+	Offsets = 
+	[[-1, -2], [-1, 2], [1, -2], [1, 2], [-2, -1], [-2, 1], [2, -1], [2, 1]]
+
+	def knight_moves
+		self.legal_moves = 
+		find_knight_moves.select do |pair|
+			pair.all? { |ordinate| ordinate.between?(1, 8) } &&
+			!friendly?(*pair)
+		end		
+	end
+
+	def find_knight_moves
+		Offsets.map do |offset|
+			[position, offset].transpose.map { |x| x.reduce(:+) }
+		end
 	end
 end
