502f2063e062a0d9a1830230340e00307ec977a9
nmacawile <nmacawile@gmail.com>
2017-03-13 04:00:31 +0800

    add local and network Player classes

---
+++ lib/player.rb
@@ -0,0 +1,79 @@
+require "socket"
+
+class Player
+	attr_accessor :game, :name, :faction
+
+	def initialize(game, name, faction)
+		@game = game
+		@name = name
+		@faction = faction
+	end
+
+	def to_s
+		"[#{faction.capitalize}]#{name}"
+	end
+
+	def input
+		print_to_screen "#{self} : "
+		get_input
+	end
+
+	def turn
+		show_turn_message
+		get_input
+	end
+
+	def get_input
+		gets.chomp.downcase
+	end
+
+	def puts_to_screen(message)
+		puts message
+	end
+
+	def print_to_screen(message)
+		print message
+	end
+
+	def show_turn_message
+		message = "#{self}'s turn. "
+		message += "You are checked! " if game.board.checked?(faction)
+		message += ": "
+		print_to_screen message
+	end
+
+end
+
+class NetworkPlayer < Player
+	attr_accessor :server, :player
+
+	def initialize(game, name, faction)
+		super
+		start_server
+		wait_for_player
+	end
+
+	def start_server
+		self.server = TCPServer.open("localhost", 3000)
+	end
+
+	def wait_for_player
+		print "Waiting for player..."
+		self.player = server.accept
+		puts "player connected"
+	end
+
+	def get_input
+		input = player.gets.chomp
+		input
+	end
+
+	def print_to_screen(message)
+		player.puts message
+	end	
+
+	def puts_to_screen(message)
+		player.puts "#{message}\n"
+	end	
+	
+end
